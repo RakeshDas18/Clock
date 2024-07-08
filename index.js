5230e5bedc50eeae11fe41ed2d29dd3dac93fcce
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-07-08 10:46:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function clock(){
                 
     var monthNames = [ "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December" ]; 
-    var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
     var today = new Date();
 
