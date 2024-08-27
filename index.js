f828fa22fd636658e871ba31573d885bb4edcb85
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-08-27 20:37:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function clock(){
                 
     var monthNames = [ "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December" ]; 
-    var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
     var today = new Date();
 
-    document.getElementById('Date').innerHTML = (dayNames[today.getDay()] + " " + 
+    document.getElementById("Date").innerHTML = (dayNames[today.getDay()] + " " + 
     today.getDate() + ' ' + monthNames[today.getMonth()] + ' ' +today.getFullYear());
 
     
